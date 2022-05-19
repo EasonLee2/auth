dab4b6b79e64551d4b6bebe1b2dbd877e5bba893
EasonLee2 <eason.lee@kellogg.northwestern.edu>
2022-05-19 02:33:02 +0000

    in class stuff done (many things unable to follow)

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,31 @@
+class SessionsController < ApplicationController
+
+    def new
+    end
+
+    def create
+        @user = User.find_by({ "email" => params["email"]})
+        if @user
+            if BCrypt::Password.new(@user["password"]) == params["password"]
+                session["user_id"] = @user["id"]
+                
+
+                flash["notice"] = "Welcome."
+                redirect_to "/companies"
+            else
+                flash["notice"] = "Nope."
+                redirect_to "/sessions/new"
+            end
+        else
+            flash["notice"] = "Nope."
+            redirect_to "/sessions/new"
+        end
+    end
+
+    def destroy
+        session["user_id"] = nil
+        flash["notice"] = "Goodbye."
+        redirect_to "/sessions/new"
+    end
+
+end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,10 +5,18 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new
-    @task["description"] = params["task"]["description"]
-    @task.save
-    redirect_to "/tasks"
+    @user = User.find_by({ "email" => params["email"]})
+    if @user
+      session["user_id"] = @user["id"]
+      flash["notice"] = "Welcome."
+      @task = Task.new
+      @task["description"] = params["task"]["description"]
+      @task.save
+      redirect_to "/tasks"
+    else
+      flash["notice"] = "Unverified User"
+      redirect_to "/tasks"
+    end
   end
 
   def destroy
